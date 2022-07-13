4cdaaba73596f3f7d4881a3e4e6dde7581d5cca3
Ozi <oguzkaanyildiz@Ozis-MacBook-Air.local>
2022-07-13 14:10:38 +0200

    Fetching and displaying monster names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+
 function App() {
+
+  const [monsters, setMonsters] = useState([])
+
+  async function getMonsters() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    setMonsters(data)
+    console.log(monsters)
+  }
+
+  getMonsters()
+
   return (
     <div>
-      App
+      {monsters.map(monster => {
+        return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        )
+      })}
     </div>
   )
 }
